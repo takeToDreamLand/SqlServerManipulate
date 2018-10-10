98c03232ed9e0041fb688fe6f9cc7d87673ffc21
takeToDreamLand <dreameameam@163.com>
2018-10-10 09:56:11 +0800

    DataConnection对象完成

---
+++ SqlSeverManipulate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetMultipulat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=ConsotoUniversityDatabase;Integrated Security=SSPI";
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConnectionString;
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (con != null) con.Close(); // 关闭连接
+            }
+        }
+    }
+}
